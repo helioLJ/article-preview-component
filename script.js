0ec79b918ed4b8b316f58bb066a4a77bcbdf8681
helioLJ <helio.lucio.jr@hotmail.com>
2022-06-10 18:43:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let share_box = document.querySelector('.share-box')
+let share_button = document.querySelector('.share-icon')
+let arrow_box = document.querySelector('.arrow')
+
+let share_box_mobile = document.querySelector('.share-box-mobile')
+let share_button_mobile = document.querySelector('.share-icon-mobile')
+
+share_button.addEventListener('click', (e) => {
+    e.preventDefault
+    share_box.classList.toggle('hide')
+    arrow_box.classList.toggle('hide')
+})
+
+share_button_mobile.addEventListener('click', (e) => {
+    e.preventDefault
+    console.log("oi");
+    share_box_mobile.classList.toggle('shows-up')
+})
